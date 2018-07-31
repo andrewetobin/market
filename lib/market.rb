aed1235878f73c37a01beeb6f044810ac1cdcdc7
andrewetobin <andrewetobin@gmail.com>
2018-07-31 10:06:32 -0600

    iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -25,8 +25,18 @@ class Market
   def sorted_item_list
     list = @vendors.map do |vendor|
       vendor.inventory.keys
+    end.flatten
+    list.uniq.sort
+  end
+
+  def total_inventory
+    total_inventory = Hash.new(0)
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, amount|
+        total_inventory[item] += amount
+      end
     end
-    list.flatten.uniq.sort
+    total_inventory
   end
 
 
